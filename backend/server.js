8d4739e0798697165157331cb324ac8be15dad25
Juan Varela <jjvalela0203@gmail.com>
2026-07-11 12:18:20 -0500

    Feat: Importations of future routes

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,6 @@ Starts Express, mounts routes and global error handlers
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import authRoutes from './src/routes/auth.routes.js'
+import userRoutes from './src/routes/user.routes.js'
+import exerciseRoutes from './src/routes/exercise.routes.js'
