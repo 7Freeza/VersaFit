1a1a00ba89097be155ee04f2ab1acf7d9b28a247
IamJuan201 <neverprobg@gmail.com>
2026-07-12 19:42:21 -0500

    Add endpoint to log in a user by checking email and password

--- backend/src/controllers/auth_controller.js
+++ backend/src/controllers/auth_controller.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { query } from '../config/db.js';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'versafit_secret_key';
+const JWT_SECRET = process.env.JWT_SECRET || 'versafit_secret_key'
 
 export async function register(req, res) {
     try {
@@ -38,6 +38,50 @@ export async function register(req, res) {
         })
     } catch (error) {
         console.error('Error during user registration:', error.message)
+        return res.status(500).json({ message: 'Internal server error' })
+    }
+}
+
+export async function login(req, res) {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' })
+        }
+
+        const result = await query('SELECT * FROM users WHERE email = $1', [email])
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({ message: 'Invalid email or password' })
+        }
+
+        const user = result.rows[0]
+
+        const isPasswordValid = await bcrypt.compare(password, user.password_hash)
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid email or password' })
+        }
+
+        const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
+            expiresIn: '1d',
+        })
+
+        return res.status(200).json({
+            message: 'Login successful',
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                weight: user.weight,
+                height: user.height,
+                goal: user.goal
+            },
+            token,
+        })
+    } catch (error) {
+        console.error('Error during user login:', error.message)
         return res.status(500).json({ message: 'Internal server error' })
     }
 }
