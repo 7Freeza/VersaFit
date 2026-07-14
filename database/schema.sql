9383e42e1b4d56ae419b6db1451ce497f8f9924e
Sauhul <164249543+Sauhul@users.noreply.github.com>
2026-07-14 09:35:08 -0500

    feat:create users table

--- database/schema.sql
+++ database/schema.sql
@@ -14,3 +14,15 @@ DROP TABLE IF EXISTS weight_logs CASCADE;
 DROP TABLE IF EXISTS physical_profiles CASCADE;
 DROP TABLE IF EXISTS objectives CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
+
+--Create tables
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255)UNIQUE NOT NULL,
+    password_hash VARCHAR(255)NOT NULL,
+    full_name VARCHAR(100)NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
