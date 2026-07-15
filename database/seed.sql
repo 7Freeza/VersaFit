f84cac5a11c17652267242b5552ea43ccc8f1ba7
Sauhul <164249543+Sauhul@users.noreply.github.com>
2026-07-15 09:44:15 -0500

    insert data user demo

--- database/seed.sql
+++ database/seed.sql
@@ -7,3 +7,13 @@ INSERT INTO objectives (name , description) VALUES
   ('Build muscle','Increase muscle mass and strengh'),
   ('Improve cardio','Improve health and cardiovascular endurance'),
   ('Stay active','Stay active and reduce stress');
+
+--USER DEMO PASSWORD (BCRYPT HASH)
+
+INSERT INTO users (email, password_hash, full_name)VALUES
+  {
+    'dem@versafit.com',
+    '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'
+    'Demo User'
+  };
+
